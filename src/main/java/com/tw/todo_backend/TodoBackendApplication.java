717958e4a1e0c12bbe5e6200177cf0af7ced0b3d
Adarsh A <adarsh.a@thoughtworks.com>
2022-07-03 17:35:41 +0530

    [Adarsh] Refactor. Remove unwanted test and imports

--- src/main/java/com/tw/todo_backend/TodoBackendApplication.java
+++ src/main/java/com/tw/todo_backend/TodoBackendApplication.java
@@ -3,8 +3,6 @@ package com.tw.todo_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Map;
-
 @SpringBootApplication
 public class TodoBackendApplication {
 

bf4015d1b87dde93a4de9097c1c00caaa550934a
Adarsh A <adarsh.a@thoughtworks.com>
2022-07-05 21:26:46 +0530

    [Adarsh] Add. Cors support for frontend

--- src/main/java/com/tw/todo_backend/TodoBackendApplication.java
+++ src/main/java/com/tw/todo_backend/TodoBackendApplication.java
@@ -2,6 +2,10 @@ package com.tw.todo_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class TodoBackendApplication {
@@ -10,4 +14,13 @@ public class TodoBackendApplication {
         SpringApplication.run(TodoBackendApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
